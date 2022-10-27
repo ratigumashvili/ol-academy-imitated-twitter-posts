2573be4fb284727916bc16179f04d0a2adca0ce8
Rati <ratigumashvili@gmail.com>
2022-10-27 12:07:09 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Feed from "./components/Feed";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Feed />
+    </div>
+  );
+}
+
+export default App;
